496703bf5bf7bb8a1a56c8ce51fbcb7e4e5d1138
TIVMOF <tomyivanov06@gmail.com>
2024-12-04 07:03:50 +0000

    feat: New Dashboard Structure

--- codbex-vacations/navigations/leave-requests/service.js
+++ codbex-vacations/navigations/leave-requests/service.js
@@ -2,7 +2,7 @@ const navigationData = {
     id: 'leave-requests-navigation',
     label: "Leave Requests",
     group: "employees",
-    order: 1000,
+    order: 800,
     link: "/services/web/codbex-vacations/gen/codbex-vacations/ui/LeaveRequests/index.html?embedded"
 };
 
