bc01e4df1758ffc0e9fd54084211c12e7f5840d5
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-01-07 16:17:03 +0200

    add .mjs file

---
+++ codbex-vacations/scheduled-jobs/AnnualLeaveBalance/annual-leave-balance-handler.mjs
@@ -0,0 +1,36 @@
+import { AnnualLeaveBlanceService } from './AnnualLeaveBlanceService';
+
+import { EmployeeRepository } from "codbex-employees/gen/codbex-employees/dao/Employees/EmployeeRepository";
+import { LeaveBalanceRepository } from "codbex-vacations/gen/codbex-vacations/dao/LeaveBalance/LeaveBalanceRepository";
+import { EmployeeContractRepository } from "codbex-contracts/gen/codbex-contracts/dao/EmployeeContracts/EmployeeContractRepository";
+
+
+const EmployeeContractDao = new EmployeeContractRepository();
+const LeaveBalanceDao = new LeaveBalanceRepository();
+const EmployeeDao = new EmployeeRepository();
+
+const employees = EmployeeDao.findAll();
+
+employees.forEach((employee) => {
+
+    const leaveBalancesCount = LeaveBalanceDao.count();
+
+    const employeeContract = EmployeeContractDao.findAll({
+        $filter: {
+            equals: {
+                Employee: employee.Id
+            }
+        }
+    });
+
+
+    const leaveBalance = {
+        "id": leaveBalancesCount + 1,
+        "employee": employee.Id,
+        "year": new Date().getFullYear(),
+        "granted": employeeContract[0].AnnualPaidLeave,
+        "used": 0
+    }
+
+    AnnualLeaveBlanceService.insertLeaveBalance(leaveBalance);
+});
