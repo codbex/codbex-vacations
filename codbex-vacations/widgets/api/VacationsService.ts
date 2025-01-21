a10756c0159dbc9472cd00337b1c221f2e959973
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-01-21 20:36:26 +0200

    add my leave balances widget

--- codbex-vacations/widgets/api/VacationsService.ts
+++ codbex-vacations/widgets/api/VacationsService.ts
@@ -1,6 +1,46 @@
-import { Controller, } from "sdk/http";
+import { Controller, Get } from "sdk/http";
+import { user } from "sdk/security";
+
+import { EmployeeRepository as EmployeeDao } from "codbex-employees/gen/codbex-employees/dao/Employees/EmployeeRepository";
+import { LeaveBalanceRepository as LeaveBalanceDao } from "codbex-vacations/gen/codbex-vacations/dao/LeaveBalance/LeaveBalanceRepository";
+
 
 @Controller
 class VacationsService {
+
+    private readonly employeeDao;
+    private readonly leaveBalanceDao;
+
+    constructor() {
+        this.employeeDao = new EmployeeDao();
+        this.leaveBalanceDao = new LeaveBalanceDao();
+    }
+
+    @Get("/User")
+    public GetLoggedInUser() {
+
+        const users = this.employeeDao.findAll({
+            $filter: {
+                equals: {
+                    Email: user.getName()
+                }
+            }
+        });
+
+        const leaveBalances = this.leaveBalanceDao.findAll({
+            $filter: {
+                equals: {
+                    Employee: users[0].Id
+                }
+            }
+        });
+
+        const remainingLeave = leaveBalances.reduce((sum, lb) => sum + lb.Balance, 0);
+
+
+        return {
+            "RemainingLeave": remainingLeave
+        };
+    }
 
 }
--- codbex-vacations/widgets/my-leave-balance/controller.js
+++ codbex-vacations/widgets/my-leave-balance/controller.js
@@ -14,20 +14,22 @@ angular.module('my-leave-balance', ['ideUI', 'ideView'])
 
         $scope.currentYear = new Date().getFullYear();
 
-        // const salariesServiceUrl = "/services/ts/codbex-salaries/widgets/api/SalariesService.ts/SalariesSum";
+        const salariesServiceUrl = "/services/ts/codbex-vacations/widgets/api/VacationsService.ts/User";
 
-        // $http.get(salariesServiceUrl)
-        //     .then(function (response) {
-        //         console.log(response.data);
-        //         $scope.salariesSum = response.data.salariesSum;
-        //         $scope.currency = response.data.currency;
-        //     })
-        //     .catch(function (error) {
-        //         $scope.state.error = true;
-        //         $scope.state.isBusy = false;
-        //         console.error('Error fetching data:', error);
-        //     })
-        //     .finally(function () {
-        //         $scope.state.isBusy = false;
-        //     });
+        $http.get(salariesServiceUrl)
+            .then(function (response) {
+                console.log(response.data);
+                $scope.RemainingLeave = response.data.RemainingLeave;
+            })
+            .catch(function (error) {
+                $scope.state.error = true;
+                $scope.state.isBusy = false;
+                console.error('Error fetching data:', error);
+            })
+            .finally(function () {
+                $scope.state.isBusy = false;
+            });
+
+
+
     }]);
