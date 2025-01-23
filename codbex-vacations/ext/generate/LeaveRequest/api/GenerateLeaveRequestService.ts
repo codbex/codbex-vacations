f09dbd92191245e1be9d78fd37107f15a5c047a7
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-01-23 16:41:46 +0200

    generate leave request with the new form

--- codbex-vacations/ext/generate/LeaveRequest/api/GenerateLeaveRequestService.ts
+++ codbex-vacations/ext/generate/LeaveRequest/api/GenerateLeaveRequestService.ts
@@ -1,8 +1,76 @@
-import { Controller } from "sdk/http";
+import { Controller, Get } from "sdk/http";
+import { user } from "sdk/security";
+
+import { EmployeeRepository as EmployeeDao } from "codbex-employees/gen/codbex-employees/dao/Employees/EmployeeRepository";
+import { JobAssignmentRepository as JobAssignmentDao } from "codbex-jobs/gen/codbex-jobs/dao/Employees/JobAssignmentRepository";
+import { JobPositionRepository as JobPositionDao } from "codbex-jobs/gen/codbex-jobs/dao/Teams/JobPositionRepository";
+import { TeamRepository as TeamDao } from "codbex-organizations/gen/codbex-organizations/dao/Teams/TeamRepository";
 
 @Controller
 class GenerateLeaveRequestService {
 
+    private readonly employeeDao;
+    private readonly jobAssignmentDao;
+    private readonly jobPositionDao;
+    private readonly teamDao;
 
+    constructor() {
+        this.employeeDao = new EmployeeDao();
+        this.jobAssignmentDao = new JobAssignmentDao();
+        this.jobPositionDao = new JobPositionDao();
+        this.teamDao = new TeamDao();
+    }
+
+    @Get("/Employee")
+    public getLoggedEmployee() {
+
+        const employees = this.employeeDao.findAll({
+            $filter: {
+                equals: {
+                    Email: user.getName()
+                }
+            }
+        });
+
+        return { "Employee": employees[0].Id }
+    }
+
+    @Get("/Manager")
+    public getEmployeeManager() {
+
+        const employees = this.employeeDao.findAll({
+            $filter: {
+                equals: {
+                    Email: user.getName()
+                }
+            }
+        });
+
+        const jobAssignments = this.jobAssignmentDao.findAll({
+            $filter: {
+                equals: {
+                    Employee: employees[0].Id
+                }
+            }
+        });
+
+        const jobPositions = this.jobPositionDao.findAll({
+            $filter: {
+                equals: {
+                    Id: jobAssignments[0].JobPosition
+                }
+            }
+        });
+
+        const teams = this.teamDao.findAll({
+            $filter: {
+                equals: {
+                    Id: jobPositions[0].Team
+                }
+            }
+        });
+
+        return { "Manager": teams[0].Manager }
+    }
 
 }
--- codbex-vacations/ext/generate/LeaveRequest/leave-request-generate-controller.js
+++ codbex-vacations/ext/generate/LeaveRequest/leave-request-generate-controller.js
@@ -1,13 +1,80 @@
 const app = angular.module('templateApp', ['ideUI', 'ideView']);
 
-app.controller('templateController', ['$scope', '$http', 'ViewParameters', 'messageHub', function ($scope, $http, ViewParameters, messageHub) {
-    const params = ViewParameters.get();
+app.controller('templateController', ['$scope', '$http', 'messageHub', function ($scope, $http, messageHub) {
     $scope.showDialog = true;
+
+    $scope.entity = {};
+    $scope.forms = {
+        details: {},
+    };
+
+    const leaveTypesUrl = "/services/ts/codbex-vacations/gen/codbex-vacations/api/entities/LeaveTypeService.ts/";
+    const employeeUrl = "/services/ts/codbex-vacations/ext/generate/LeaveRequest/api/GenerateLeaveRequestService.ts/Employee";
+    const managerUrl = "/services/ts/codbex-vacations/ext/generate/LeaveRequest/api/GenerateLeaveRequestService.ts/Manager";
+    const leaveRequestUrl = "/services/ts/codbex-vacations/gen/codbex-vacations/api/LeaveRequests/LeaveRequestService.ts/";
+
+    $http.get(leaveTypesUrl)
+        .then(function (response) {
+            const leaveTypesOptions = response.data;
+
+            $scope.OptionsLeaveType = leaveTypesOptions.map(function (value) {
+                return {
+                    value: value.Id,
+                    text: value.Name
+                };
+            });
+        });
+
+
+    $scope.generateLeaveRequest = function () {
+
+        const startDate = $scope.entity.StartDate;
+        const endDate = $scope.entity.EndDate;
+        const days = $scope.entity.Days;
+        const type = $scope.entity.Type;
+        const reason = $scope.entity.Reason;
+
+        $http.get(employeeUrl)
+            .then(function (response) {
+                const employee = response.data.Employee;
+
+                $http.get(managerUrl)
+                    .then(function (response) {
+                        const manager = response.data.Manager;
+
+                        const leaveRequest =
+                        {
+                            "Employee": employee,
+                            "Manager": manager,
+                            "StartDate": startDate,
+                            "EndDate": endDate,
+                            "Days": days,
+                            "Type": type,
+                            "Status": 1,
+                            "Reason": reason,
+                        }
+
+                        console.log(leaveRequest);
+
+                        $http.post(leaveRequestUrl, leaveRequest)
+                            .then(function (response) {
+                                console.log(response.data);
+                                $scope.closeDialog();
+                            })
+                            .catch(function (error) {
+                                console.error("Error creating Leave Request", error);
+                                $scope.closeDialog();
+                            });
+                    })
+            });
+
+        messageHub.showAlertSuccess("LeaveRequest", "Leave Request successfully created");
+    }
 
 
     $scope.closeDialog = function () {
         $scope.showDialog = false;
-        messageHub.closeDialogWindow("leave-deduction-generate");
+        messageHub.closeDialogWindow("leave-request-generate");
     };
 
     document.getElementById("dialog").style.display = "block";
