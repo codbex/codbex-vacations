c9fa5f13d253d5329c9c860cd2296feae8d8846b
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-02-02 16:46:19 +0200

    add count of leave requests on the widget

--- codbex-vacations/widgets/api/VacationsService.ts
+++ codbex-vacations/widgets/api/VacationsService.ts
@@ -3,6 +3,7 @@ import { user } from "sdk/security";
 
 import { EmployeeRepository as EmployeeDao } from "codbex-employees/gen/codbex-employees/dao/Employees/EmployeeRepository";
 import { LeaveBalanceRepository as LeaveBalanceDao } from "codbex-vacations/gen/codbex-vacations/dao/LeaveBalance/LeaveBalanceRepository";
+import { LeaveRequestRepository as LeaveRequestDao } from "codbex-vacations/gen/codbex-vacations/dao/LeaveRequests/LeaveRequestRepository";
 
 
 @Controller
@@ -10,10 +11,12 @@ class VacationsService {
 
     private readonly employeeDao;
     private readonly leaveBalanceDao;
+    private readonly leaveRequestDao;
 
     constructor() {
         this.employeeDao = new EmployeeDao();
         this.leaveBalanceDao = new LeaveBalanceDao();
+        this.leaveRequestDao = new LeaveRequestDao();
     }
 
     @Get("/User")
@@ -37,9 +40,17 @@ class VacationsService {
 
         const remainingLeave = leaveBalances.reduce((sum, lb) => sum + lb.Balance, 0);
 
+        const leaveRequests = this.leaveRequestDao.findAll({
+            $filter: {
+                equals: {
+                    Employee: users[0].Id
+                }
+            }
+        });
 
         return {
-            "RemainingLeave": remainingLeave
+            "RemainingLeave": remainingLeave,
+            "LeaveRequests": leaveRequests.length
         };
     }
 
--- codbex-vacations/widgets/my-leave-requests/controller.js
+++ codbex-vacations/widgets/my-leave-requests/controller.js
@@ -6,6 +6,21 @@ angular.module('my-leave-requests', ['ideUI', 'ideView'])
             busyText: "Loading...",
         };
 
+        const salariesServiceUrl = "/services/ts/codbex-vacations/widgets/api/VacationsService.ts/User";
+
+        $http.get(salariesServiceUrl)
+            .then(function (response) {
+                console.log(response.data);
+                $scope.LeaveRequests = response.data.LeaveRequests;
+            })
+            .catch(function (error) {
+                $scope.state.error = true;
+                $scope.state.isBusy = false;
+                console.error('Error fetching data:', error);
+            })
+            .finally(function () {
+                $scope.state.isBusy = false;
+            });
 
 
     }]);
