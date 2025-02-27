67483df8fc3ea0288f2b60cf3013c8aa91ee9f4e
tivmof <tomyivanov06@gmail.com>
2025-02-27 12:10:05 +0000

    feat: Hnadling notifications to employees

---
+++ codbex-vacations/events/NotifySender/NotifySenderHandler.ts
@@ -0,0 +1,34 @@
+import { process } from "sdk/bpm";
+import { EmployeeRepository } from "codbex-employees/gen/codbex-employees/dao/Employees/EmployeeRepository";
+import { LeaveStatusRepository } from "codbex-vacations/gen/codbex-vacations/dao/entities/LeaveStatusRepository";
+
+export const trigger = (event) => {
+
+    const EmployeeDao = new EmployeeRepository();
+    const LeaveStatusDao = new LeaveStatusRepository();
+
+    if (event.operation === "update") {
+        const leaveRequest = event.entity;
+
+        console.log("Here event notify");
+
+        const employee = EmployeeDao.findById(leaveRequest.Employee);
+        const manager = EmployeeDao.findById(leaveRequest.Manager);
+        const requestId = leaveRequest.Id;
+        const status = leaveRequest.Status
+
+        const statusName = LeaveStatusDao.findById(status).Name;
+
+        const processInstanceId = process.start("approve-request", {
+            RequestId: requestId,
+            From: manager.Email,
+            To: employee.Email,
+            Status: statusName
+        });
+
+        if (processInstanceId == null) {
+            console.log("Failed to create opportunity action process!");
+            return;
+        }
+    }
+}
--- codbex-vacations/tasks/notify-approvers.ts
+++ codbex-vacations/tasks/notify-approvers.ts
@@ -5,16 +5,45 @@ import { sendMail } from "./mail-util"
 const execution = process.getExecutionContext();
 const executionId = execution.getId();
 
-const requester = process.getVariable(executionId, "From");
+const requester = process.getVariable(executionId, "To");
+const sender = process.getVariable(executionId, "From");
 const requestId = process.getVariable(executionId, "RequestId");
-const managersEmail = config.get("LEAVE_REQUEST_MANAGERS_EMAIL", "managers-dl@example.com");
+const status = process.getVariable(executionId, "Status");
 
-const subject = "New leave request";
+if (!status) {
+    const managersEmail = config.get("LEAVE_REQUEST_MANAGERS_EMAIL", "managers-dl@example.com");
 
-const approvalLink = "http://localhost:8080/services/web/codbex-vacations/ext/generate/LeaveDeduction/leave-deduction-generate.html?id=" + requestId;
+    const subject = "New leave request";
 
-const content = `<h4>A new leave request for [${requester}] has been created</h4>Open the inbox <a href="${approvalLink}" target="_blank">here</a> to process the request.`;
+    const approvalLink = "http://localhost:8080/services/web/codbex-vacations/ext/generate/LeaveDeduction/leave-deduction-generate.html?id=" + requestId;
 
-console.log("Here notify approvers");
+    const content = `<h4>A new leave request for [${requester}] has been created</h4>Open the inbox <a href="${approvalLink}" target="_blank">here</a> to process the request.`;
 
-sendMail(managersEmail, subject, content);
+    console.log("Here notify approvers");
+
+    sendMail(managersEmail, subject, content);
+}
+else {
+    if (status === "Approved") {
+        const managersEmail = config.get("LEAVE_REQUEST_EMPLOYEE_EMAIL", requester)
+
+        const subject = "Leave Request Approved";
+
+        const content = `<h4>Your leave request has been approved by ${sender}</h4>`;
+
+        console.log("Here notify approvers");
+
+        sendMail(managersEmail, subject, content);
+    }
+    if (status === "Rejected") {
+        const managersEmail = config.get("LEAVE_REQUEST_EMPLOYEE_EMAIL", requester)
+
+        const subject = "Leave Request Denied";
+
+        const content = `<h4>Your leave request has been denied by ${sender}</h4>`;
+
+        console.log("Here notify approvers");
+
+        sendMail(managersEmail, subject, content);
+    }
+}
