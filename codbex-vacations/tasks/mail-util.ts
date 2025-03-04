ece47ed24a19a543e2484b0692a46ae2ea9881f3
tivmof <tomyivanov06@gmail.com>
2025-03-04 15:56:24 +0000

    test: Twilio email functionality

--- codbex-vacations/tasks/mail-util.ts
+++ codbex-vacations/tasks/mail-util.ts
@@ -9,20 +9,20 @@ function isMailConfigured() {
         configurations.get("DIRIGIBLE_MAIL_PASSWORD") &&
         configurations.get("DIRIGIBLE_MAIL_TRANSPORT_PROTOCOL") &&
         (
-            (configurations.get("DIRIGIBLE_MAIL_SMTPS_HOST") && configurations.get("DIRIGIBLE_MAIL_SMTPS_PORT") && configurations.get("DIRIGIBLE_MAIL_SMTPS_AUTH"))
+            (configurations.get("DIRIGIBLE_MAIL_SMTPS_HOST") && configurations.get("DIRIGIBLE_MAIL_SMTPS_PORT"))
             ||
-            (configurations.get("DIRIGIBLE_MAIL_SMTP_HOST") && configurations.get("DIRIGIBLE_MAIL_SMTP_PORT") && configurations.get("DIRIGIBLE_MAIL_SMTP_AUTH"))
+            (configurations.get("DIRIGIBLE_MAIL_SMTP_HOST") && configurations.get("DIRIGIBLE_MAIL_SMTP_PORT"))
         );
 }
 
-export function sendMail(to: string, subject: string, content: string) {
-    const from = configurations.get("LEAVE_REQUEST_APP_FROM_EMAIL", "georgievanikol36@gmail.com");
+export function sendMail(reciever: string, subject: string, content: string) {
+    const sender = configurations.get("DIRIGIBLE_MAIL_USERNAME");
 
     if (isMailConfigured()) {
-        logger.info("Sending mail to [{}] with subject [{}] and content: [{}]...", to, subject, content);
-        mailClient.send(from, to, subject, content, 'html');
+        logger.info("Sending mail to [{}] with subject [{}] and content: [{}]...", reciever, subject, content);
+        mailClient.send(sender, reciever, subject, content, 'html');
     } else {
-        logger.info("Mail to [{}] with subject [{}] and content [{}] will NOT be send because the mail client is not configured.", to, subject, content);
+        logger.info("Mail to [{}] with subject [{}] and content [{}] will NOT be send because the mail client is not configured.", reciever, subject, content);
     }
 
 }
