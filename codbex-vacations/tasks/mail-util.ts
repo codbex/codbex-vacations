8f6c6405a8d08a794c6326d5f5e315a4d572433e
tivmof <tomyivanov06@gmail.com>
2025-03-05 15:02:20 +0000

    test: Twilio

--- codbex-vacations/tasks/mail-util.ts
+++ codbex-vacations/tasks/mail-util.ts
@@ -16,7 +16,7 @@ function isMailConfigured() {
 }
 
 export function sendMail(reciever: string, subject: string, content: string) {
-    const sender = configurations.get("DIRIGIBLE_MAIL_USERNAME");
+    const sender = configurations.get("LEAVE_REQUEST_APP_FROM_EMAIL");
 
     if (isMailConfigured()) {
         logger.info("Sending mail to [{}] with subject [{}] and content: [{}]...", reciever, subject, content);
